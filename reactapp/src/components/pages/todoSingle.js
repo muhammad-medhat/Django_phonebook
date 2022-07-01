61173be33bcc375e361ef399862b0a3af3b748c6
Muhammad <Muhammad.medhat@gmail.com>
2022-07-01 15:36:31 +0200

    Editing component

--- reactapp/src/components/pages/todoSingle.js
+++ reactapp/src/components/pages/todoSingle.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editable from '../com/Editable';
 
 const TodoSingle = (props)=>  {
     const editTODO = (t)=>{
@@ -6,6 +7,10 @@ const TodoSingle = (props)=>  {
         // this.props.history.replace(`/todoform/${t.id}/${t.title}`)
     }
     const {todo, del} = props
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [task, setTask] = useState(todo);
+
         const t = todo
        console.log(`props ${t.id}`, props);
     //    console.log('single task', t);
@@ -13,8 +18,45 @@ const TodoSingle = (props)=>  {
             <>
             <tr id={t.id}>
                 <td>{t.id}</td>
-                <td>{t.title}</td>
-                <td>{t.description}</td>
+                <td>
+                    {/* {t.title} */}
+                    <Editable
+                        text={t.title} 
+                        placeholder="Write a task name"
+                        childRef={`${t.id}-title`}
+                        type="input"
+                    >
+                    <input
+                        ref={`${t.id}-title`}
+
+                        type="text"
+                        name="task"
+                        placeholder="Write a task name"
+                        value={t.title}
+                        onChange={e => setTask({...task, title: e.target.value})}
+                    />
+                    </Editable>
+                
+                </td>
+                <td>
+                    {/* {t.description} */}
+                    <Editable
+                    text={t.description}
+                    placeholder="Description for the task"
+                    type="textarea"
+                    childRef={`${t.id}-description`}
+
+                    >
+                          <textarea
+                            name="description"
+                            ref={`${t.id}-description`}
+                            placeholder="Description for the task"
+                            rows="5"
+                            value={t.description}
+                            onChange={e => setTask({...task, description: e.target.value})}
+                        />
+                    </Editable>
+                </td>
 
                 <td onClick={()=>editTODO(t)}>ee
                     <i className="fas fa-edit" ></i>
